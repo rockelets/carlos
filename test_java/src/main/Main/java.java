de6b7d82bc0196ccf328fce01e9f4a1f9ae872f4
rockelets <carlos.moscoso12@hotmail.com>
2017-11-02 10:04:52 -0300

    Ahora

--- test_java/src/main/Main/java.java
+++ test_java/src/main/Main/java.java
@@ -6,6 +6,10 @@ public class java {
     
     public static void main(String[] args) {
         System.out.println("Hola mundo desde java!");
+        
+        Cliente c = new Cliente(1,"Carlos");
+        
+        System.out.println(c.getNombre);
     }
     
 }
